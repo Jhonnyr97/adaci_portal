229accc6ea26dcda6429040eeb446a72e44c9113
suraj solanki <suraj.solanki@bytestechnolab.insuraj.solanki@bytestechnolab.in>
2022-07-13 05:53:35 +0000

    create wp-admin custom menu & overwrite learndash registration form

--- wp-content/themes/assurena-child/inc/email-reminder/current-working.php
+++ wp-content/themes/assurena-child/inc/email-reminder/current-working.php
@@ -11,12 +11,78 @@ add_action( 'admin_menu', 'adaci_email_reminder_working_postion' );
 /**
  * Display a custom menu page
  */
-function adaci_email_reminder_menu_page(){
-    esc_html_e( 'Admin Page Test', 'textdomain' );  
+function adaci_email_reminder_menu_page(){  ?>
+	<div class="notice notice-success is-dismissible" style="display: none;"> 
+		<p><strong>Settings saved.</strong></p>
+		<button type="button" class="notice-dismiss">
+			<span class="screen-reader-text">Dismiss this notice.</span>
+		</button>
+	</div>
+	<h1 class="wp-heading-inline">Email Notification For Working Position</h1>
+
+    <form action="<?php echo admin_url('admin.php?page=email-reminder'); ?>" method="post" name="email_reminder_form" id="email_reminder_form">
+        <table class="form-table">
+        	 <tr class="form-field form-required">
+                <th scope="row"><label for="email_send"><?php _e('Email Send'); ?></label></th>
+                <td><input name="email_send" type="radio" id="email_send" value="before" aria-required="true" <?php echo (get_option('email_send') == 'before') ? "checked" : ""; ?> /><label for="before"><?php _e('Before'); ?></label>&nbsp;
+                	<input name="email_send" type="radio" id="email_send" value="after" aria-required="true" <?php echo (get_option('email_send') == 'after') ? "checked" : ""; ?> /><label for="before"><?php _e('After'); ?></label></td>
+            </tr>
+            <tr class="form-field">
+                <th scope="row"><label for="send_mail_days"><?php _e('Select Day Before or After '); ?></label></th>
+                <td><select name="send_mail_days" id="send_mail_days">
+                    <?php
+
+
+                    for ($i=1; $i <=30 ; $i++) { 
+                    	if ( $i == get_option('send_mail_days')) // preselect specified role
+                            $p = "\n\t<option selected='selected' value='" . esc_attr($i) . "'>$i</option>";
+                        else
+                            $r .= "\n\t<option value='" . esc_attr($i) . "'>$i</option>";
+                    }
+                    echo $p . $r;
+                    ?>
+                    </select>
+                </td>
+            </tr>
+            <tr class="form-field form-required">
+                <th scope="row"><label for="email_subject"><?php _e('Email Subject'); ?></label></th>
+                <td><input name="email_subject" type="text" id="email_subject" value="<?php echo get_option('email_subject'); ?>" aria-required="true" style="width: 35%;"/></td>
+            </tr>
+            <tr class="form-field form-required">
+                <th scope="row"><label for="email_short_content"><?php _e('Email Short Content'); ?></label></th>
+                <td><textarea name="email_short_content" type="text" id="email_short_content" cols="5" rows="8" style="width: 35%;"> <?php echo get_option('email_short_content'); ?> </textarea>  </td>
+            </tr>
+        </table>
+        <?php submit_button( __( 'Save '), 'primary', 'emailremindersave' ); ?>
+    </form> <?php 
+
+    if(isset($_POST['emailremindersave'])){ 
+
+    	update_option( 'email_send', $_POST['email_send'] );
+    	update_option( 'send_mail_days', $_POST['send_mail_days'] );
+    	update_option( 'email_subject', $_POST['email_subject'] );
+    	update_option( 'email_short_content', $_POST['email_short_content'] );
+    	update_option( 'email_reminder_message', 1);
+    	wp_safe_redirect(admin_url('admin.php?page=email-reminder&message-email=success'));
+    	
+    }
+    	
+
+    
+
+}
+if($_REQUEST['message-email'] == "success" && get_option('email_reminder_message') == 1){
+	update_option( 'email_reminder_message', 0);
+	add_action( 'admin_notices', 'sample_admin_notice__success' );
 }
 
-
-
+function sample_admin_notice__success() {
+    ?>
+    <div class="notice notice-success is-dismissible">
+        <p><strong><?php _e( 'Settings Successfully Saved.', 'sample-text-domain' ); ?></strong></p>
+    </div>
+    <?php
+}
 
 
 function adaci_check_mail_send($userID,$companyTable,$mailBetweenDay,$emailReminderSend){
@@ -33,9 +99,11 @@ function adaci_check_mail_send($userID,$companyTable,$mailBetweenDay,$emailRemin
 	$display_name = ($theUser->data->display_name) ? $theUser->data->display_name : get_user_meta($userID,'first_name',true)." ".get_user_meta($userID,'first_name',true);
 
 	$to = $email;
-    $subject = 'Current Working Postion';
+    $subject = get_option('email_subject');
     $message = '<p>Hi ,'.$display_name.'</p>
-	<p>You working on <strong>'.$result[0]->business_name.'</strong> Company last 1 year so kindly update your current working position if needed.</p>';
+	<p>You working on <strong>'.$result[0]->business_name.'</strong> Company last 1 year so kindly update your current working position if needed.</p>'
+	.get_option('email_short_content');
+
 
     $result = wp_mail( $to, $subject, $message );      
     if($result){
@@ -93,7 +161,5 @@ function every_twenty_four_event_working_position_function() {
 
 				}
 			}
-	
-	
-    
+  
 }
